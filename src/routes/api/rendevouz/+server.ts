a4871cdad6590c06526d14401224f55e8a5479d4
Andrew Allen <andrew@Andrews-MacBook-Air.local>
2023-09-26 21:47:52 +0100

    keep ngrok happy

--- src/routes/api/rendevouz/+server.ts
+++ src/routes/api/rendevouz/+server.ts
@@ -3,15 +3,18 @@ import { resend } from "$lib/server/resend";
 import { json } from "@sveltejs/kit";
 
 export async function POST({ request }) {
+  console.log(request.body);
   const { id } = await request.body;
   const plan = await prisma.plan.findUnique({
     where: { id },
-    include: { recipients: true }
+    include: { recipients: true },
   });
 
   if (plan) {
-    let emails : string[] = [];
-    plan.recipients.map(r => { emails = [...emails, r.email] });
+    let emails: string[] = [];
+    plan.recipients.map((r) => {
+      emails = [...emails, r.email];
+    });
     const resend_data = await resend.emails.send({
       from: "Rendevouz <onboarding@resend.dev>",
       to: emails,
@@ -21,7 +24,7 @@ export async function POST({ request }) {
       <p>${plan.description}</p>
       <p>Participants: ${emails.toString()}</p>
       <p>TIME: ${plan.time}</p>
-      `
+      `,
     });
   }
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
+    // keep ngrok happy
+    // see https://github.com/sveltejs/kit/issues/8026#issuecomment-1605293431
+    // plus https://stackoverflow.com/a/76018697/4711754
+    // tldr: `vite dev` doesn't use node adapter that sets `PROTOCOL_HEADER`, `HOST_HEADER` and `ORIGIN` headers
+    csrf: {
+      checkOrigin: process.env.NODE_ENV === "development" ? false : true,
+    },
+  },
 };
 
 export default config;
