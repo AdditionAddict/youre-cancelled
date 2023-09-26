1d48159eb5fbadc1014947d9ba9cdeb628d26614
Andrew Allen <andrew@Andrews-MacBook-Air.local>
2023-09-26 21:42:04 +0100

    keep typescript happy

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,51 +5,59 @@ import { QSTASH_TOKEN } from "$env/static/private";
 export const actions = {
   sendPlan: async ({ request }) => {
     const form_data = await request.formData();
-    const email_1 = form_data.get("email_1");
-    const email_2 = form_data.get("email_2");
-    const description = form_data.get("description");
-    const time = form_data.get("time");
+    const emailOneFormValue = form_data.get("email_1");
+    const emailTwoFormValue = form_data.get("email_2");
+    const descriptionFormValue = form_data.get("description");
+    const timeFormValue = form_data.get("time");
+
+    if (emailOneFormValue === null) throw new Error("email_1 is null");
+    if (emailTwoFormValue === null) throw new Error("email_2 is null");
+    if (descriptionFormValue === null) throw new Error("description is null");
+    if (timeFormValue === null) throw new Error("time is null");
+
+    const emailOne = emailOneFormValue.toString();
+    const emailTwo = emailTwoFormValue.toString();
+    const description = descriptionFormValue.toString();
+    const time = timeFormValue.toString();
 
     const plan_data = await prisma.plan.create({
       data: {
         description,
         time: new Date(time).toISOString(),
         recipients: {
-          create: [
-            { email: email_1 },
-            { email: email_2 }
-          ]
-        }
-      }
+          create: [{ email: emailOne }, { email: emailTwo }],
+        },
+      },
     });
 
     console.log({ plan_data });
 
     const resend_data = await resend.emails.send({
       from: "Plans <onboarding@resend.dev>",
-      to: [ email_1, email_2 ],
+      to: [emailOne, emailTwo],
       subject: description,
       html: `
         <strong>PLANS MADE</strong>
         <p>${description}</p>
-        <p>Participants: ${email_1}, ${email_2}</p>
+        <p>Participants: ${emailOne}, ${emailTwo}</p>
         <p>TIME: ${time}</p>
-      `
+      `,
     });
-
 
     console.log({ resend_data });
 
-
-    const qstash_data = await fetch("https://qstash.upstash.io/v1/publish/https://youre-cancelled.vercel.app/api/rendevouz", {
-      method: "POST",
-      headers: {
-        "Authorization": `Bearer ${QSTASH_TOKEN}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email: email_1 })
-    });
+    const qstash_data = await fetch(
+      "https://qstash.upstash.io/v1/publish/https://a897-86-4-63-62.ngrok-free.app/api/rendevouz",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${QSTASH_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: emailOne }),
+      }
+    );
 
     console.log({ qstash_data });
   },
-}
+};
